f302649fc6df927d588678bf8dc084b1c7244f72
syndersage <syndersage@mail.ru>
2022-06-24 20:18:13 +0700

    init commit

---
+++ src/main/java/com/lessons/lesson02/Example2.java
@@ -0,0 +1,17 @@
+package com.lessons.lesson02;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int var1;
+        int var2;
+
+        var1 = 1024;
+
+        System.out.println("First variable is: " + var1);
+
+        var2 = var1 / 2;
+
+        System.out.println("Second variable is first variable divided by 2: ");
+        System.out.println(var2);
+    }
+}
