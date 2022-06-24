a66177826e88f7a14a710f71ca4a90704e4bc220
syndersage <syndersage@mail.ru>
2022-06-24 20:38:01 +0700

    init commit

---
+++ src/main/java/com/lessons/lesson03/Example3.java
@@ -0,0 +1,17 @@
+package com.lessons.lesson03;
+
+public class Example3 {
+    public static void main(String[] args) {
+        int v;
+        double x;
+        v = 10;
+        x = 10.0;
+        System.out.println("Original value of X: " + x);
+        System.out.println("Original value of V: " + v);
+        System.out.println();
+        v = v / 4;
+        x = x / 4;
+        System.out.println("Changed value of X: " + x);
+        System.out.println("Changed value of V: " + v);
+    }
+}
