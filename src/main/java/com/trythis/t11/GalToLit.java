b2029bfb425dd003b8654a8517813140f3320b54
syndersage <syndersage@mail.ru>
2022-06-26 21:00:34 +0700

    Gallon to Litters two Classes

---
+++ src/main/java/com/trythis/t11/GalToLit.java
@@ -0,0 +1,16 @@
+package com.trythis.t11;
+
+import java.util.Scanner;
+
+public class GalToLit {
+    public static void main(String[] args) {
+        System.out.print("Input gallons as integer value: ");
+        Scanner scan = new Scanner(System.in);
+        while (!scan.hasNextInt()) {
+            scan.next();
+            System.out.print("Input gallons as integer value: ");
+        }
+        double gallons = scan.nextInt();
+        System.out.println(gallons + " gallons is " + gallons * 3.7854 + " litters");
+    }
+}
---
+++ src/main/java/com/trythis/t12/GalToLitTable.java
@@ -0,0 +1,23 @@
+package com.trythis.t12;
+
+import java.util.Scanner;
+
+public class GalToLitTable {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input first value of interval: ");
+        int left = 1, right = 2;
+        if (scan.hasNextInt()) {
+            left = scan.nextInt();
+        } else {
+            scan.next();
+        }
+        System.out.print("Input last value of interval: ");
+        if (scan.hasNextInt()) {
+            right = scan.nextInt();
+        }
+        for (int i = left; i < right; i++) {
+            System.out.println(i + " gallons is " + i * 3.7854 + " litters");
+        }
+    }
+}
