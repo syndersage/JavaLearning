bc9eec4a654f678740019b2d5805dcbada472fb2
syndersage <syndersage@mail.ru>
2022-06-26 23:59:26 +0700

    Added Litterals class

---
+++ src/main/java/com/randomtasks/Litterals.java
@@ -0,0 +1,13 @@
+package com.randomtasks;
+
+public class Litterals {
+    public static void main(String[] args) {
+        long l = 10L;
+        float f = 5.51F;
+        int i = 10_5_4; //equals 1054
+        i = 0xFFF;
+        i = 0123;
+        i = 0b0101011;
+        System.out.println("text" + '\b');
+    }
+}
