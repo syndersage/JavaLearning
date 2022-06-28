f7748616cea289b5427ff973b778ade207ad9f3c
syndersage <syndersage@mail.ru>
2022-06-29 00:50:25 +0700

    Enhanced LetterGuess class (works until guess)

--- src/main/java/com/randomtasks/LetterGuess.java
+++ src/main/java/com/randomtasks/LetterGuess.java
@@ -5,12 +5,17 @@ import java.io.IOException;
 public class LetterGuess {
     public static void main(String[] args) throws IOException {
         char ch, answer = 'k';
-        System.out.print("Enter the letter i made wish for: ");
-        if (answer == (ch = (char) System.in.read())) System.out.println("***RIGHT***");
-        else {
-            System.out.print("wrong, ");
-            if (ch < answer) System.out.println("answer is higher");
-            else System.out.println("answer is lower");
-        }
+        do {
+            System.out.print("Enter the letter i made wish for: ");
+            do {
+                ch = (char) System.in.read();
+            } while (ch == '\n');
+            if (answer == ch) System.out.println("***RIGHT***");
+            else {
+                System.out.print("wrong, ");
+                if (ch < answer) System.out.println("answer is higher");
+                else System.out.println("answer is lower");
+            }
+        } while (ch != answer);
     }
 }
