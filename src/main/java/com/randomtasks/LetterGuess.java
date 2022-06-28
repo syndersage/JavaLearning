1585694a2edbf7684ab270f8f0950bf73951af40
syndersage <syndersage@mail.ru>
2022-06-29 00:57:54 +0700

    Changed LetterGuess class input reader (ignore letters after the first one)

--- src/main/java/com/randomtasks/LetterGuess.java
+++ src/main/java/com/randomtasks/LetterGuess.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 
 public class LetterGuess {
     public static void main(String[] args) throws IOException {
-        char ch, answer = 'k';
+        char ch, ignore, answer = 'k';
         do {
             System.out.print("Enter the letter i made wish for: ");
+            ch = (char) System.in.read();
             do {
-                ch = (char) System.in.read();
-            } while (ch == '\n');
+                ignore = (char) System.in.read();
+            } while (ignore != '\n');
             if (answer == ch) System.out.println("***RIGHT***");
             else {
                 System.out.print("wrong, ");
