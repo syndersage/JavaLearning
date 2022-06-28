9a805ca9617404834e6aae09455f41ac26c27fef
syndersage <syndersage@mail.ru>
2022-06-28 21:13:49 +0700

    Enhanced TypesConvertion class

--- src/main/java/com/randomtasks/TypesConvertion.java
+++ src/main/java/com/randomtasks/TypesConvertion.java
@@ -14,5 +14,20 @@ public class TypesConvertion {
         System.out.println(d);
         byte b2 = (byte) 128;
         System.out.println(b2);
+        byte b3 = 10;
+        l = (byte) (b3 + b3);
+        System.out.println(((Object) l).getClass().getSimpleName());
+        char ch2 = 129;
+        System.out.println((short) ch2);
+        int i1 = 10, i2 = 3;
+        System.out.println((double) (i1 / 3));
+        for (int j = 0; j < 5; j++) {
+            System.out.println(j + " " + (double) (j / 3));
+        }
+        double d2 = 50;
+        //d2 = (int) (d2 + 5.5);
+        System.out.println(((Object) ((int) d2 + 5.5)).getClass().getSimpleName());
+        int a = (int) d2++;
+        System.out.println(a + " " + d2);
     }
 }
