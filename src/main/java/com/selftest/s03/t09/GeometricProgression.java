ccbab18b69486e9be54cff4adf5b6ce29bede73c
syndersage <syndersage@mail.ru>
2022-07-01 01:21:22 +0700

    Added classes from chapter 3 tasks

---
+++ src/main/java/com/selftest/s03/t09/GeometricProgression.java
@@ -0,0 +1,12 @@
+package com.selftest.s03.t09;
+
+public class GeometricProgression {
+    public static void main(String[] args) {
+        int result = 1;
+        while (true) {
+            result += result;
+            if (result > 10000) break;
+            System.out.println(result);
+        }
+    }
+}
