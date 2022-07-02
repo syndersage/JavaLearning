0f96ea924dd14a9653e3ff3eb1b32b820cd4926b
syndersage <syndersage@mail.ru>
2022-07-03 03:15:31 +0700

    Added ConsoleArguments class

---
+++ src/main/java/com/randomtasks/ConsoleArguments.java
@@ -0,0 +1,24 @@
+package com.randomtasks;
+
+public class ConsoleArguments {
+    public static void main(String[] args) {
+        String[][] contactBook = {
+                {"Ivan", "12345"},
+                {"Boris", "00000"},
+                {"Anna", "99511"}
+        };
+        if (args.length != 1) {
+            System.out.println("Usage: java ConsoleArguments <name>");
+            return;
+        }
+        boolean found = false;
+        for (String[] contact : contactBook) {
+            if (contact[0].equals(args[0])) {
+                System.out.println(contact[0] + " number is: " + contact[1]);
+                found = true;
+                break;
+            }
+        }
+        if (!found) System.out.println(args[0] + " phone number not found.");
+    }
+}
