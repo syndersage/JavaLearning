1f8e7e44d51c1441072edd3811b14ff3edfe862b
syndersage <syndersage@mail.ru>
2022-07-03 00:40:18 +0700

    Added ArrayDemo class

---
+++ src/main/java/com/randomtasks/ArrayDemo.java
@@ -0,0 +1,20 @@
+package com.randomtasks;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] arr = {99, -10, 100123, 18, -978, 5623, 463, -9, 287, 49};
+        int size = 10;
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
