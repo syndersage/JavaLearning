b9c5a2feff4a8e0938b674d763748af673afba6f
syndersage <syndersage@mail.ru>
2022-07-03 19:52:43 +0700

    Added BitwiseOperations class

---
+++ src/main/java/com/randomtasks/BitwiseOperations.java
@@ -0,0 +1,37 @@
+package com.randomtasks;
+
+class ShowBits {
+    int maxBitsLength;
+
+    ShowBits(int maxBitsLength) {
+        if (maxBitsLength >= 64) {
+            this.maxBitsLength = 63;
+            if (maxBitsLength > 64) System.out.println("Bit length changed to max value: 64");
+        }
+        else this.maxBitsLength = maxBitsLength;
+    }
+
+    String getBits(long valueToShow) {
+        String bitsAsString = "";
+        int spacer = 0, spaceEveryNSymbols = 8;
+        for (long i = (1L << (maxBitsLength - 1)); i > 0; i >>>= 1, spacer++) {
+            if (spacer == 8) {
+                bitsAsString = bitsAsString.concat(" ");
+                spacer = 0;
+            }
+            if ((valueToShow & i) == 0) bitsAsString = bitsAsString.concat("0");
+            else bitsAsString = bitsAsString.concat("1");
+        }
+//        while (bitsAsString.length() > 8 && bitsAsString.startsWith("00000000")) {
+//            bitsAsString = bitsAsString.substring(9);
+//        }
+        return bitsAsString;
+    }
+}
+
+public class BitwiseOperations {
+    public static void main(String[] args) {
+        ShowBits showbits = new ShowBits(100);
+        System.out.println(showbits.getBits(9_223_372_036_854_775_807L));
+    }
+}
