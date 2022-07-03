330c45db35187c5d68ecd5ba8e87d5b2dd54d85c
syndersage <syndersage@mail.ru>
2022-07-04 03:59:13 +0700

    Added AverageArrayValue, StringBubbleSorting, XORCipher classes.

---
+++ src/main/java/com/selftest/s05/t03/AverageArrayValue.java
@@ -0,0 +1,12 @@
+package com.selftest.s05.t03;
+
+
+public class AverageArrayValue {
+    public static void main(String[] args) {
+        double[] arr = {1, 5, 100, 22, 15, 0.5, -11.78, -765, 105.6, -55.565439104};
+        double average = 0;
+        for (var elem : arr) average += elem;
+        average /= arr.length;
+        System.out.println(average);
+    }
+}
