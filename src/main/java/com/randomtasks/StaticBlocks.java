c869c4d35425fcd3c02cf04c03c7822e11b4907a
syndersage <syndersage@mail.ru>
2022-07-07 19:50:31 +0700

    Changed StaticBlock class
    testing static blocks

---
+++ src/main/java/com/randomtasks/StaticBlocks.java
@@ -0,0 +1,20 @@
+package com.randomtasks;
+
+class StaticBlock {
+    static int a;
+
+    public int getA() {
+        return a;
+    }
+
+    static {
+        StaticBlock.a = 10;
+        System.out.println("HHE");
+    }
+}
+
+public class StaticBlocks {
+    public static void main(String[] args) {
+        StaticBlock.a = 15;
+    }
+}
