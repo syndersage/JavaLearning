60117c1f7f784abe021b2fa42df81aca7fd4b8b3
syndersage <syndersage@mail.ru>
2022-07-14 21:33:06 +0700

    Added TestArraycopy and changed InterfaceQueueDemo classes

---
+++ src/main/java/com/randomtasks/TestArraycopy.java
@@ -0,0 +1,7 @@
+package com.randomtasks;
+
+public class TestArraycopy {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+    }
+}
--- src/main/java/com/trythis/c08/t01/InterfaceQueueDemo.java
+++ src/main/java/com/trythis/c08/t01/InterfaceQueueDemo.java
@@ -1,15 +1,16 @@
 package com.trythis.c08.t01;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 interface Queue {
-    int defaultSize = 10;
     int defaultStartIndex = 0;
     String defaultName = "Default";
     void push(int x);
     default void emptyQueuePop() { System.out.println("Action failed! Queue is empty!"); }
     default void fullQueuePush() { System.out.println("Action failed! Queue is full!"); }
     int pop();
+    void resetQueue();
     String getQueueName();
 }
 
@@ -67,29 +68,12 @@ class LinearQueue implements Queue {
     public boolean lastActionValid() {
         return validAction;
     }
-}
 
-class QueueMenu {
-
-    static void QueueTypes() {
-        System.out.print("""
-                Which type of queue create:
-                \t1. Linear
-                \t2. Circular
-                Choose one (0 to quit):\s""");
-    }
-
-    static boolean isValidType(int clause) { return clause <= 2 && clause >= 0; }
-
-    static void QueueOptions() {
-        System.out.print("""
-                What to do with queue:
-                \t1. Push
-                \t2. Pop
-                Choose one (0 to quit):\s""");
-    }
-    static boolean isValidQueue(int clause) {
-        return clause >= 0 && clause <= 2;
+    @Override
+    public void resetQueue() {
+        Arrays.fill(arr, 0);
+        index = 0;
+        System.out.println("Queue successfully reset");
     }
 }
 
@@ -122,6 +106,117 @@ class CircularQueue implements Queue {
     }
 
     public String getQueueName() { return name; }
+
+    @Override
+    public void resetQueue() {
+        Arrays.fill(arr, 0);
+        index = 0;
+        System.out.println("Queue successfully reset");
+    }
+}
+
+class DynamicQueue implements Queue {
+    protected final int change;
+    protected int[] arr;
+    public final static int defaultChange = 2;
+    private final int defaultSize;
+    protected int index;
+    private final String name;
+    public void increaseQueue() {
+        var tempArr = new int[arr.length + change];
+        System.arraycopy(arr, 0, tempArr, 0, arr.length);
+        arr = tempArr;
+        System.out.println("Queue size has been increased");
+    }
+    DynamicQueue(int size, int change, String name) {
+        this.change = change;
+        this.arr = new int[size];
+        this.index = defaultStartIndex;
+        this.name = name;
+        this.defaultSize = arr.length;
+    }
+
+    DynamicQueue(int size, String name) {
+        this(size, defaultChange, name);
+    }
+
+    DynamicQueue(int size) {
+        this(size, defaultChange, defaultName);
+    }
+
+    public void push(int x) {
+        if (index == arr.length) increaseQueue();
+        arr[index++] = x;
+    }
+
+    public int pop() {
+        if (index == 0) return -1;
+        return arr[--index];
+    }
+
+    public String getQueueName() {
+        return name;
+    }
+
+    public void resetQueue() {
+        arr = new int[defaultSize];
+        index = 0;
+        System.out.println("Queue successfully reset");
+    }
+}
+
+class DecreaseAbleDynamicQueue extends DynamicQueue {
+    DecreaseAbleDynamicQueue(int size) {
+        super(size);
+    }
+    DecreaseAbleDynamicQueue(int size, String name) {
+        super(size, name);
+    }
+    DecreaseAbleDynamicQueue(int size, int change, String name) {
+        super(size, change, name);
+    }
+
+    public void decreaseQueue() {
+        var tempArr = new int[arr.length - change];
+        System.arraycopy(arr, 0, tempArr, 0, arr.length - change);
+        arr = tempArr;
+        System.out.println("Queue size has been decreased");
+    }
+
+    @Override
+    public int pop() {
+        if (index == 0) return -1;
+        var tempReturn = arr[--index];
+        if (index == arr.length - change) decreaseQueue();
+        return tempReturn;
+    }
+}
+
+class QueueMenu {
+
+    static void QueueTypes() {
+        System.out.print("""
+                Which type of queue create:
+                \t1. Linear
+                \t2. Circular
+                \t3. Dynamic
+                \t4. Dynamic (decrease able)
+                Choose one (0 to quit):\s""");
+    }
+
+    static boolean isValidType(int clause) { return clause <= 4 && clause >= 0; }
+
+    static void QueueOptions() {
+        System.out.print("""
+                What to do with queue:
+                \t1. Push
+                \t2. Pop
+                \t3. Reset
+                Choose one (0 to quit):\s""");
+    }
+    static boolean isValidQueue(int clause) {
+        return clause >= 0 && clause <= 3;
+    }
 }
 
 public class InterfaceQueueDemo {
@@ -134,8 +229,14 @@ public class InterfaceQueueDemo {
             menuItem = scan.nextInt();
         } while (!QueueMenu.isValidType(menuItem));
         switch (menuItem) {
+            case 0 -> {
+                System.out.println("Quiting...");
+                return;
+            }
             case 1 -> queue = new LinearQueue(4, "Linear 1");
             case 2 -> queue = new CircularQueue(3, "Circular 1");
+            case 3 -> queue = new DynamicQueue(3, "Dynamic 1");
+            case 4 -> queue = new DecreaseAbleDynamicQueue(2, 3, "DecreaseAbleDynamic");
             default -> {
                 System.out.println("Incorrect menu item!");
                 return;
@@ -146,7 +247,10 @@ public class InterfaceQueueDemo {
             do {
                 menuItem = scan.nextInt();
             } while (!QueueMenu.isValidQueue(menuItem));
-            if (menuItem == 0) break;
+            if (menuItem == 0) {
+                System.out.println("Quiting...");
+                break;
+            }
             switch (menuItem) {
                 case 1 -> {
                     System.out.print("Enter integer value to add in " + queue.getQueueName() + " queue: ");
@@ -156,6 +260,7 @@ public class InterfaceQueueDemo {
                     int popped = queue.pop();
                     System.out.println("Obtained value from " + queue.getQueueName() + " queue: " + popped);
                 }
+                case 3 -> queue.resetQueue();
                 default ->  System.out.println("Incorrect menu item!");
             }
         }
